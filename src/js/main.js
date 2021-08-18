869cb7e827b52081d89c7c7d742c0e2f8688634d
segatt0 <anabellesegatto@hotmail.com>
2021-08-18 15:30:12 -0300

    feat: tudo

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+var hippoMouse = document.querySelector('.refH1');
+// console.log(hippoMouse);
+hippoMouse.addEventListener(
+  'mouseover',
+  function () {
+    hippoMouse.classList.add('animate__bounce');
+  },
+  false
+);
+hippoMouse.addEventListener(
+  'mouseout',
+  function () {
+    hippoMouse.classList.remove('animate__bounce');
+  },
+  false
+);
